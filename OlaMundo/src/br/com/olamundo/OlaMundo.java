d7ef2ce95f07d434afeefcd3e0b1b7970203e08d
Amilton <amiltonmelo.dev@hotmail.com>
2022-05-10 12:05:54 -0300

    Commit initial

---
+++ OlaMundo/src/br/com/olamundo/OlaMundo.java
@@ -0,0 +1,13 @@
+//Pacote criado
+package br.com.olamundo;
+
+//Classe criada
+public class OlaMundo {
+	
+	//Execução do código
+	public static void main(String[]args) {
+		
+		//Impressão da frase no console
+		System.out.print("Olá Mundo");
+	}
+}
